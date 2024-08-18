540298ccee1f6083b7af28f611949a3256f775c6
Priyadharshini <priyadharshinik1720@gmail.com>
2024-08-18 16:33:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 function calculate()
 {
     const birth=document.getElementById("date1").value;
-    const current=document.getElementById("date2").value;
 
-    if(!birth || !current)
+    if(!birth)
     {
         alert("Invalid Input...");
         return;
     }
 
     const birth1=new Date(birth);
-    const current1=new Date(current);
+    const now=new Date();
 
-    let age=current1.getFullYear()-birth1.getFullYear();
-    const monthd=current1.getMonth()-birth1.getMonth();
-    const dated=current1.getDate()-birth1.getDate();
+    let age=now.getFullYear()-birth1.getFullYear();
+    const monthd=now.getMonth()-birth1.getMonth();
+    const dated=now.getDate()-birth1.getDate();
 
     if (monthd < 0 || (monthd===0  && dated < 0)) 
     {
